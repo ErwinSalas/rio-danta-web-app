186273011d8ac6e0d96599e41fc46795d52d668b
Baxi19 <randald1991@gmail.com>
2018-02-03 09:56:53 -0600

    Update Chat

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default () => (
     
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
       <div class="fixed-action-btn horizontal">
-      <iframe src="https://tawk.to/chat/5a754fbf4b401e45400ca2e8/default/?$_tawk_popout=true" height={320} width={300} />
+      <iframe src="https://tawk.to/chat/5a754fbf4b401e45400ca2e8/default/?$_tawk_popout=true" height={300} width={300} />
       <Link href="/">
       <a class="btn-floating btn-large color-danton ">
         
@@ -29,13 +29,18 @@ export default () => (
       </a>
       </Link>
   
+    
+    <ul>
+      <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+      <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+      <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
+      <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+    </ul>
   </div>
     
     <Header 
       title="Hostel Río Danta "
-      su
-      
-      btitle="Hospedaje y Tours En El Mismo Lugar A Un Precio Accesible"
+      subtitle="Hospedaje y Tours En El Mismo Lugar A Un Precio Accesible"
       btntitle="Habitaciones"
       classRef="#header"
     
